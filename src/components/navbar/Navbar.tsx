bade66622639451db696946c7650545e4a018e93
ronymia <mdronymia.javascript@gmail.com>
2024-07-26 20:11:53 +0600

    navbar complete

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,61 @@
+import { Link, NavLink } from "react-router-dom";
+import "./navbar.scss";
+import { useState } from "react";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const [open, setOpen] = useState(false);
+
+  const user = false;
+  return (
+    <nav>
+      <div className="left">
+        <Link to={"/"} className="logo">
+          <img src="./logo.png" alt="" />
+          <span>DreamHome</span>
+        </Link>
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+        <NavLink to={"/contact"}>Contact</NavLink>
+        <NavLink to={"/agent"}>Agent</NavLink>
+      </div>
+
+      <div className="right">
+        {user ? (
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            <span>John Doe</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <NavLink to="/">Sign in</NavLink>
+            <NavLink to="/" className="register">
+              Sign up
+            </NavLink>
+          </>
+        )}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/about"}>About</NavLink>
+          <NavLink to={"/contact"}>Contact</NavLink>
+          <NavLink to={"/agent"}>Agents</NavLink>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
+import "./rootLayout.scss";
 
 export default function RootLayout() {
   return (
